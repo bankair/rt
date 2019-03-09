0bbed48c5d8d11b2f6dfc3cadad86e5d888303d1
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2019-03-09 08:52:58 +0100

    Add more color methods

--- features/step_definitions/color.rb
+++ features/step_definitions/color.rb
@@ -15,3 +15,22 @@ end
 Then("c.blue = {float}") do |float|
   expect(@c.blue).to eq(float)
 end
+
+Given("c{int} ← color {float}, {float}, {float}") do |int, float, float2, float3|
+  (@c ||= {})[int] = Color.new(float, float2, float3)
+end
+
+Then("c{int} + c{int} = color {float}, {float}, {float}") do |int, int2, float, float2, float3|
+  expect(@c[int] + @c[int2]).to eq(Color.new(float, float2, float3))
+end
+
+Then("c{int} - c{int} = color {float}, {float}, {float}") do |int, int2, float, float2, float3|
+  color = @c[int] - @c[int2]
+  expect(color.red).to be_within(0.00001).of(float)
+  expect(color.green).to be_within(0.00001).of(float2)
+  expect(color.blue).to be_within(0.00001).of(float3)
+end
+
+Then("c * {int} = color {float}, {float}, {float}") do |int, float, float2, float3|
+  expect(@c * int).to eq(Color.new(float, float2, float3))
+end
--- lib/color.rb
+++ lib/color.rb
@@ -7,4 +7,31 @@ class Color
     self.green = green
     self.blue = blue
   end
+
+  def ==(other)
+    return false unless other.is_a?(self.class)
+    red == other.red && green == other.green && blue == other.blue
+  end
+
+  def +(others)
+    Array(others).reduce(self) do |result, color|
+      self.class.new(
+        result.red + color.red,
+        result.green + color.green,
+        result.blue + color.blue
+      )
+    end
+  end
+
+  def -(other)
+    self.class.new(
+      red - other.red,
+      green - other.green,
+      blue - other.blue
+    )
+  end
+
+  def *(scalar)
+    self.class.new(red * scalar, green * scalar, blue * scalar)
+  end
 end
